e927b222ceb156f01996d0c06f71bb7a57c08acb
aliefibnu <aliefarumi@gmail.com>
2024-10-09 11:13:25 +0700

    buat algoritma hitung luas segitiga

---
+++ tugas_skaju/buk_sulis/flowchart.js
@@ -0,0 +1,14 @@
+// ? Deklarasi Variabel
+let alas, tinggi, hasil;
+
+// ? Masukkan input
+alas = prompt("Masukkan panjang alas segitiga");
+tinggi = prompt("Masukkan tinggi segitiga");
+
+// ? Hitung Luas Segitiga
+hasil = (1 / 2) * alas * tinggi;
+
+// ? Tampilkan data
+alert(
+  `Luas alas segitiga dari alas : ${alas} dan tinggi ${tinggi} adalah ${hasil}`
+);
